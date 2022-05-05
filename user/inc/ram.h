ddbb4eb329bf0b28d171e8eae9b3840133ee495e
bin9215 <qlwyb@aoyagi-cq.com>
2022-05-05 13:13:41 +0800

    2022.05.05  添加#define STX0011  0定义，用于测试模式区分三点式送信机和一点式送信机。

--- user/inc/ram.h
+++ user/inc/ram.h
@@ -6,11 +6,14 @@
 #include "Type.h" //
 #include "key_and_Other.h"
 
+#define BUNKA_STX    1  //0用于新生精; 1用于文化(Bunka)送信机STX0031
+#define STX0011      0  //0用于三点式送信机(STX0031), 1用于一点式送信机(STX0011)
+
 extern xdata u16 X_COUNT;
 extern xdata u16 X_ERR; //璁板綍閿欒鐨勪釜鏁?
 extern xdata u8  X_ERR_CNT;
 extern xdata u16 X_ERRTimer;
-
+ 
 extern xdata u8 SIO_cnt;
 extern xdata u8 SIO_buff[16];
 extern xdata u8 SIO_DATA[16];
